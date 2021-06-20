c0b01e6c899b9fad8b50c3fd8dd51fc475b922d2
Lucas Luis Ribeiro <lucas.luisribeiro@gmail.com>
2021-06-20 18:34:34 -0300

    Comit Inicial

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,125 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable no-console */
+import React, { useState, useEffect } from 'react';
+
+import income from '../../assets/income.svg';
+import outcome from '../../assets/outcome.svg';
+import total from '../../assets/total.svg';
+
+import api from '../../services/api';
+
+import Header from '../../components/Header';
+
+import formatValue from '../../utils/formatValue';
+
+import { Container, CardContainer, Card, TableContainer } from './styles';
+
+interface Transaction {
+  id: string;
+  title: string;
+  value: number;
+  formattedValue: string;
+  formattedDate: string;
+  type: 'income' | 'outcome';
+  category: { title: string };
+  created_at: Date;
+}
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+interface Transactions {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
+const Dashboard: React.FC = () => {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [balance, setBalance] = useState<Balance>({} as Balance);
+
+  useEffect(() => {
+    async function loadTransactions(): Promise<void> {
+      const response = await api.get<Transactions>(`transactions`);
+      console.log(response.data);
+      setBalance(response.data.balance);
+
+      const transactionlist = response.data.transactions;
+
+      await Promise.all(
+        transactionlist.map(async trans => {
+          trans.formattedValue = formatValue(trans.value);
+          const d = new Date(trans.created_at);
+          trans.formattedDate = `${d.getDate()}/${d.getMonth()}/${d.getFullYear()}`;
+        }),
+      );
+
+      setTransactions(transactionlist);
+    }
+
+    loadTransactions();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <CardContainer>
+          <Card>
+            <header>
+              <p>Entradas</p>
+              <img src={income} alt="Income" />
+            </header>
+            <h1 data-testid="balance-income">
+              R$ {formatValue(balance.income)}
+            </h1>
+          </Card>
+          <Card>
+            <header>
+              <p>Saídas</p>
+              <img src={outcome} alt="Outcome" />
+            </header>
+            <h1 data-testid="balance-outcome">
+              R$ {formatValue(balance.outcome)}
+            </h1>
+          </Card>
+          <Card total>
+            <header>
+              <p>Total</p>
+              <img src={total} alt="Total" />
+            </header>
+            <h1 data-testid="balance-total">R$ {formatValue(balance.total)}</h1>
+          </Card>
+        </CardContainer>
+
+        <TableContainer>
+          <table>
+            <thead>
+              <tr>
+                <th>Título</th>
+                <th>Preço</th>
+                <th>Categoria</th>
+                <th>Data</th>
+              </tr>
+            </thead>
+            <tbody>
+              {transactions.map(trans => (
+                <tr>
+                  <td className="title">{trans.title}</td>
+                  <td className={trans.type}>R$ {trans.formattedValue}</td>
+                  <td>{trans.category.title}</td>
+                  <td>{trans.formattedDate}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </TableContainer>
+      </Container>
+    </>
+  );
+};
+
+export default Dashboard;
